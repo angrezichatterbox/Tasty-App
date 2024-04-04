233ad65303c5028f70c1809d63698b2db64e28ee
angrezichatterbox <gouthammohanraj@gmail.com>
2024-04-04 13:07:41 +0530

    Initial Commit

---
+++ Tasty/TastyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastyApp.swift
+//  Tasty
+//
+//  Created by Gautham Mohanraj on 04/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TastyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
